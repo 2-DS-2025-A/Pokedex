875eaa10d3547fb1eda54961dc6df4b67d4b9044
GalloJunior <gallojunior@gmail.com>
2025-09-08 09:56:42 -0300

    Finalizando a Home e iniciando Details

--- Pokedex/Controllers/HomeController.cs
+++ Pokedex/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Pokedex.Data;
 using Pokedex.Models;
 
 namespace Pokedex.Controllers;
@@ -7,13 +9,29 @@ namespace Pokedex.Controllers;
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private readonly AppDbContext _db;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(
+        ILogger<HomeController> logger,
+        AppDbContext db
+    )
     {
         _logger = logger;
+        _db = db;
     }
 
     public IActionResult Index()
+    {
+        var pokemons = _db.Pokemons
+            .Include(p => p.Regiao)
+            .Include(p => p.Genero)
+            .Include(p => p.Tipos)
+            .ThenInclude(t => t.Tipo)
+            .ToList();
+        return View(pokemons);
+    }
+
+    public IActionResult Details(uint id)
     {
         return View();
     }
